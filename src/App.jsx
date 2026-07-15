959d49d8e1307827fe2bbfebaa8b69063953e156
Amaya Davis <aedavis2005@gmail.com>
2026-07-15 02:04:39 -0500

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import TodoList from './TodoList';
-import TodoForm from './TodoForm';
+import TodoList from './TodoList.jsx';
+import TodoForm from './TodoForm.jsx';
 
 function App() {
  
 
   return (
     <div>
-      <h1>My Todos</h1>
+      <h1>Todo List</h1>
       <TodoForm />
       <TodoList />
       
